8384d586c2e6962a7b0c4c7bbf2c8c51d575ddb0
neccccc <nec@shell8.net>
2022-08-03 15:43:52 +0200

    wip

--- src/components/heropage/index.js
+++ src/components/heropage/index.js
@@ -2,13 +2,13 @@ import Button from '../button'
 import * as css from './heropage.module.scss'
 import * as dayjs from 'dayjs'
 import 'dayjs/locale/hu'
-import React from 'react'
 import { Link } from 'gatsby'
+import React from 'react'
 
 const HeroPage = ({ title, lead, date, buttonText = '', buttonLink = '' }) => (
   <article className={css.heroPage}>
     <h2 className={css.title}>
-      {buttonLink ? (<Link to={buttonLink}>{title}</Link>) : title}
+      {buttonLink ? <Link to={buttonLink}>{title}</Link> : title}
     </h2>
     <p className={css.date}>
       {dayjs(date).locale('hu').format('YYYY. MMMM DD.')}
