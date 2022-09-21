115cd662770a9b9da3a13f4a79f4b9db4d19d7ad
Szabolcs Szabolcsi-Toth <nec@shell8.net>
2022-09-21 20:40:38 +0200

    lint

--- src/utils/getImageSize.js
+++ src/utils/getImageSize.js
@@ -2,11 +2,15 @@ const getImageSize = (gatsbyImage) => {
   const landscape = gatsbyImage.width > gatsbyImage.height
   const max = Math.max(gatsbyImage.width, gatsbyImage.height)
   const min = Math.min(gatsbyImage.width, gatsbyImage.height)
-  const ratio = min/max
+  const ratio = min / max
 
   return {
-    height: !landscape ? 'calc(100vh - 8rem)' : `calc(calc(90vw - 4rem) * ${ratio})`,
-    width: landscape ? 'calc(90vw - 4rem)' : `calc(calc(100vh - 8rem) * ${ratio}`,
+    height: !landscape
+      ? 'calc(100vh - 8rem)'
+      : `calc(calc(90vw - 4rem) * ${ratio})`,
+    width: landscape
+      ? 'calc(90vw - 4rem)'
+      : `calc(calc(100vh - 8rem) * ${ratio}`,
   }
 }
 
