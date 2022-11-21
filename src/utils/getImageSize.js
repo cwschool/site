f509d41ece72c243a3b8abc10e968e0f28d5772a
Szabolcs Szabolcsi-Toth <szabolcs.szabolcsi-toth@binx.hu>
2022-11-21 08:16:52 +0100

    prettify

--- src/utils/getImageSize.js
+++ src/utils/getImageSize.js
@@ -1,12 +1,15 @@
 const getImageSize = (gatsbyImage) => {
-
   const landscape = gatsbyImage.width > gatsbyImage.height
   const max = Math.max(gatsbyImage.width, gatsbyImage.height)
   const min = Math.min(gatsbyImage.width, gatsbyImage.height)
   const ratio = min / max
 
-  const width = landscape ? `calc((100vw - 14rem))` : `calc((100vh - 10rem) * ${ratio})`
-  const height = landscape ? `calc((100vw - 14rem) * ${ratio})` : `calc((100vh - 10rem))`
+  const width = landscape
+    ? `calc((100vw - 14rem))`
+    : `calc((100vh - 10rem) * ${ratio})`
+  const height = landscape
+    ? `calc((100vw - 14rem) * ${ratio})`
+    : `calc((100vh - 10rem))`
 
   return {
     height,
